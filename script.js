0d744f3f19b2bec146ac0d2df818aeb1f3cd9646
salemamerfarouk <92227852+salemamerfarouk@users.noreply.github.com>
2021-10-09 21:31:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function gitvalue(){
+    var Amont=document.getElementById("num11").value;
+    var Rate=document.getElementById("num12").value;
+    var Year =document.getElementById("num13").value;
+    var cal=Amont *Rate*Year;
+    var calculat=cal /100;
+   // console.log(calculat);"If you deposid"
+   document.write("If you deposid"); document.write(Amont);
+   document.write("at an intersetrate of "); document.write(Rate);
+  
+   document.write("you will recive an amont of");document.write(calculat);
+   document.write("in the year");
+    document.write(Year);
+
+  
